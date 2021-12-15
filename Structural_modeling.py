27692f4f74040d8bebd464384a4cf670ed7a1eb7
Krzysztof Kumka <73432969+KrzysztofKumka@users.noreply.github.com>
2021-12-15 10:58:01 +0100

    Add files via upload

---
+++ Structural_modeling.py
@@ -0,0 +1,33 @@
+#ex1
+#
+# from myhdl import block, Signal
+#
+# @block
+# def top():
+#
+#     din = Signal(0)
+#     dout = Signal(0)
+#     clk = Signal(bool(0))
+#     reset = Signal(bool(0))
+#
+#     channel_inst = channel(dout, din, clk, reset)
+#
+#     return channel_inst
+
+
+#ex2
+from myhdl import block, Signal
+
+@block
+def top(n=8):
+
+    din = [Signal(0) for i in range(n)]
+    dout = [Signal(0) for j in range(n)]
+    clk = Signal(bool(0))
+    reset = Signal(bool(0))
+    channel_inst = [None for i in range(n)]
+
+    for i in range(n):
+        channel_inst[i] = channel(dout[i], din[i], clk, reset)
+
+    return channel_inst
